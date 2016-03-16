11b3ba17cad00ee89e55901c390a92b5fba7895f
ahurtado <andhdo@gmail.com>
2016-03-15 22:02:02 -0500

    adicion de indice + funcion para encontrar derivada

--- unlvr_data/scripts/10_indexes.sql
+++ unlvr_data/scripts/10_indexes.sql
@@ -10,4 +10,7 @@ create index idx_estimated_returns_brand_id_year on estimated_returns(brand_id,y
 create index idx_financial_data_brand_id_year    on financial_data(brand_id,year) using BTREE;
 create index idx_media_data_brand_id_year        on media_data(brand_id,year) using BTREE;
 create index idx_tracking_data_brand_id_year     on tracking_data(brand_id,year) using BTREE;
-create index idx_category_data_year_cat_id       on category_data (year, cat_id) using BTREE;
+create index idx_category_data_year_cat_id       on category_data (year, cat_id) algorithm default lock default;
+/* */
+create index idx_brands_cat_id  				 on brands (cat_id) algorithm default lock default;
+
